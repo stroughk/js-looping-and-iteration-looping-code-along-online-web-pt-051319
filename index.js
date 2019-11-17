56d58d43e86ce31103349a32c73c4ccc4c58a1ae
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-17 13:08:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const names = ["Lisa", "Kaitlin", "Jan"];
 function writeCards(names) {
   for (let i = 0; i < names.length; i++)
   {
-    console.log('Thank you, ${names[i], for the wonderful birthday gift!}');
+    console.log('Thank you, ${names[i], for the wonderful surprise gift!}');
   }
   return names;
 }
