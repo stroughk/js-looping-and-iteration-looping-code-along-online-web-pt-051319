5a26f052d338af8710fee375890c13b25ad456c9
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-17 13:12:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function writeCards( namesArray, event ) {
   }
   return thankYouCards
 }
+
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
