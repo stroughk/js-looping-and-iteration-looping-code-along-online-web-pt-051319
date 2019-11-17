3fc6ee17fb931fe9ce9a11d3cfb7a7d28727d1e5
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-17 13:05:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const names = ["Ada", "Brendan", "Ali"];
+const names = ["Lisa", "Kaitlin", "Jan"];
 
 function writeCards(names) {
   for (let i = 0; i < names.length; i++)
